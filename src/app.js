3ce61e74dab06ef7262f7fe135b86f8451b95e66
panahlimurad <panahli.murad@gmail.com>
2023-03-28 21:39:54 +0400

    continue

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// API
+// http://books.google.com/books/content?id=8MzovgEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api
+// AIzaSyAkvx - sx9SeDZe - FkrmgSgY35x6zuMSEQw;
+
+$(document).ready(function () {
+    
+
+})
